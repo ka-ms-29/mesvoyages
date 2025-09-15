4783239a74983d4901b34cb65b0c84899e1a7ef5
Mostaghfera Jan <Mostaghfera Jan@192.168.1.128>
2025-09-15 18:07:47 +0200

    Phase 8 : ORM (tri des enregistrements).

--- src/Controller/VoyagesContoller.php
+++ src/Controller/VoyagesContoller.php
@@ -22,26 +22,43 @@ use Symfony\component\Routing\Annotation\Route;
 class VoyagesContoller extends AbstractController {
     #[Route('/voyages', name: 'voyages')]
     
+    /**
+     * 
+     * @return Response
+     */
     public function index(): Response{
-        $visites = $this->repository->findAll();
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
         return $this->render ("pages/voyages.html.twig", [
             'visites' =>$visites
         ]);
     }
-    
     /**
- * @var VisiteRepository
- */
-private $repository;
+     * 
+     * @param type $champ
+     * @param type $ordre
+     * @return Response
+     */
+    #[Route('/voyages/tri/{champ}/{ordre}', name: 'voyages.sort')]
+    public function sort($champ, $ordre): Response{
+        $visites = $this->repository->findAllOrderBy($champ, $ordre);
+        return $this->render("pages/voyages.html.twig", [
+            'visites' => $visites
+        ]);
+    }
+    /**
+     * @var VisiteRepository
+     */
+    private $repository;
 
-/**
- * 
- * @param VisiteRepository $repository
- */
-public function __construct(VisiteRepository $repository)
-{
-    $this->repository = $repository;
-}
+    /**
+     * 
+     * @param VisiteRepository $repository
+     */
+    public function __construct(VisiteRepository $repository)
+    {
+        
+        $this->repository = $repository;
+    }
 }
 
 
